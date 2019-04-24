be54577981b560969d325b44e0832085076c23d9
AEpro <48339908+AEpro@users.noreply.github.com>
2019-04-24 11:17:41 +0800

    Add files via upload

---
+++ [EncV1]Cyrust.lua
@@ -0,0 +1,60 @@
+-- Lua Enc Function V1
+-- Make by 💘 ๖ۣۜTis ๖ۣۜNquyễn 💝
+
+-- Pembukaan --
+gg[string.char(tonumber("74",16))..string.char(tonumber("6f",16))..string.char(tonumber("61",16))..string.char(tonumber("73",16))..string.char(tonumber("74",16))]('Hello Word!!')
+gg[string.char(tonumber("61",16))..string.char(tonumber("6c",16))..string.char(tonumber("65",16))..string.char(tonumber("72",16))..string.char(tonumber("74",16))]('App Name     : Free Fire Battlegrounds\nPakage Name : com.dts.freefireth\nScript By      : Cyrust Mods\nTelegram : https://t.me/dev_cyrustmod','MENU')
+Username = "cyrust"
+Password = "1"
+
+Result = gg[string.char(tonumber("70",16))..string.char(tonumber("72",16))..string.char(tonumber("6f",16))..string.char(tonumber("6d",16))..string.char(tonumber("70",16))..string.char(tonumber("74",16))]({"Username","Password"},nil,{"text","text"})
+if Result[1] == Username and Result[2] == Password then
+gg[string.char(tonumber("61",16))..string.char(tonumber("6c",16))..string.char(tonumber("65",16))..string.char(tonumber("72",16))..string.char(tonumber("74",16))]("Sucess")
+else
+gg[string.char(tonumber("61",16))..string.char(tonumber("6c",16))..string.char(tonumber("65",16))..string.char(tonumber("72",16))..string.char(tonumber("74",16))]("Username or Password Wrong")
+return
+end 
+
+while(true) do
+if gg[string.char(tonumber("69",16))..string.char(tonumber("73",16))..string.char(tonumber("56",16))..string.char(tonumber("69",16))..string.char(tonumber("73",16))..string.char(tonumber("69",16))..string.char(tonumber("62",16))..string.char(tonumber("6c",16))..string.char(tonumber("65",16))](true) then
+menuk = 1
+gg[string.char(tonumber("73",16))..string.char(tonumber("65",16))..string.char(tonumber("74",16))..string.char(tonumber("56",16))..string.char(tonumber("69",16))..string.char(tonumber("73",16))..string.char(tonumber("69",16))..string.char(tonumber("62",16))..string.char(tonumber("6c",16))..string.char(tonumber("65",16))](false)
+end
+START = 1
+function START()
+menu = gg[string.char(tonumber("63",16))..string.char(tonumber("68",16))..string.char(tonumber("6f",16))..string.char(tonumber("69",16))..string.char(tonumber("63",16))..string.char(tonumber("65",16))]({"Empty", "Empty", "Empty", "Empty", "Empty", "Empty", "Empty", "Empty", "Empty", "Exit "},nil,'Pakage    : com.dts.freefireth\nScript By : Cyrust Mods\nTelegram  : https://t.me/dev_cyrustmod')
+if menu == 1 then cyrust1() end
+if menu == 2 then ch2() end
+if menu == 3 then ch3() end
+if menu == 4 then ch4() end
+if menu == 5 then ch5() end
+if menu == 6 then ch6() end
+if menu == 7 then ch7() end
+if menu == 8 then ch8() end
+if menu == 9 then ch9() end
+if menu == 10 then os.exit() end
+if menu == nil then else end
+menuk = -1
+end
+function cyrust1()
+--gg[string.char(tonumber("73",16))..string.char(tonumber("65",16))..string.char(tonumber("74",16))..string.char(tonumber("52",16))..string.char(tonumber("61",16))..string.char(tonumber("6e",16))..string.char(tonumber("67",16))..string.char(tonumber("65",16))..string.char(tonumber("73",16))](gg.REGION_CODE_APP)
+gg[string.char(tonumber("74",16))..string.char(tonumber("6f",16))..string.char(tonumber("61",16))..string.char(tonumber("73",16))..string.char(tonumber("74",16))]("")
+end
+function ch2()
+--gg[string.char(tonumber("73",16))..string.char(tonumber("65",16))..string.char(tonumber("74",16))..string.char(tonumber("52",16))..string.char(tonumber("61",16))..string.char(tonumber("6e",16))..string.char(tonumber("67",16))..string.char(tonumber("65",16))..string.char(tonumber("73",16))](gg.REGION_ANONYMOUS)
+gg.searchNumber("7.5538861e-7F;1F:5", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1)
+gg.refineNumber("1", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1)
+gg.getResults(9000)
+gg[string.char(tonumber("65",16))..string.char(tonumber("64",16))..string.char(tonumber("69",16))..string.char(tonumber("74",16))..string.char(tonumber("41",16))..string.char(tonumber("6c",16))..string.char(tonumber("6c",16))]("10000", gg.TYPE_FLOAT)
+gg.clearResults()
+gg[string.char(tonumber("73",16))..string.char(tonumber("65",16))..string.char(tonumber("74",16))..string.char(tonumber("52",16))..string.char(tonumber("61",16))..string.char(tonumber("6e",16))..string.char(tonumber("67",16))..string.char(tonumber("65",16))..string.char(tonumber("73",16))](gg.REGION_ANONYMOUS)
+gg.searchNumber("5.9762459e-7F;1F:5", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1)
+gg.refineNumber("1", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1)
+gg.getResults(9000)
+gg[string.char(tonumber("65",16))..string.char(tonumber("64",16))..string.char(tonumber("69",16))..string.char(tonumber("74",16))..string.char(tonumber("41",16))..string.char(tonumber("6c",16))..string.char(tonumber("6c",16))]("10000", gg.TYPE_FLOAT)
+gg.clearResults()
+gg[string.char(tonumber("74",16))..string.char(tonumber("6f",16))..string.char(tonumber("61",16))..string.char(tonumber("73",16))..string.char(tonumber("74",16))]("Antena !!!Active")
+end
+
+if menuk == 1 then START() end
+end
